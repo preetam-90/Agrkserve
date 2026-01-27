5c7816f790b2d3267f3e9da547a3db76ad4f24ae
preetam-90 <preetam-90@users.noreply.github.com>
2026-01-27 22:06:37 +0530

    Local update

--- do_git.sh
+++ do_git.sh
@@ -2,9 +2,11 @@
 exec > git_op.log 2>&1
 echo "Adding and committing local changes..."
 git add .
-git commit -m "Local update"
-echo "Fetching and merging..."
-git pull origin main --no-rebase -m "Merge remote changes"
+git commit -m "Local update" || echo "Nothing to commit"
+echo "Fetching..."
+git fetch origin main
+echo "Merging..."
+git merge origin/main -m "Merge remote changes" --allow-unrelated-histories
 echo "Pushing..."
 git push origin main
 echo "Done."
